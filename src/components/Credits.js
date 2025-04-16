8c648a7db0940de7b39e0585c84dd21b836fdec5
Sabau Daniel Catalin <111538225+Sabau-Daniel@users.noreply.github.com>
2025-04-16 19:23:15 +0300

    added the import react at the top of every component to be able to host it

--- src/components/Credits.js
+++ src/components/Credits.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 export default function Credits({ restart }) {
   return (
     <div className="credits">
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 import "./index.css";
 import anca from "./poze/anca-alexandrescu.png";
 

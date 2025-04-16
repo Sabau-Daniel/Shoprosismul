f7c0ed257a1a0186e9500d8b9a38c69d2b99a6cb
Sabau Daniel Catalin <111538225+Sabau-Daniel@users.noreply.github.com>
2025-04-16 19:21:13 +0300

    Updates

---
+++ precache-manifest.74a1cac08886830f4a6613a2b109fbba.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ca918781e5068c33293b62191f2c4fd",
+    "url": "/Shoprosismul/index.html"
+  },
+  {
+    "revision": "05655f93960c8934c90d",
+    "url": "/Shoprosismul/static/css/main.77f80f7f.chunk.css"
+  },
+  {
+    "revision": "56f1224220a5f06f9596",
+    "url": "/Shoprosismul/static/js/2.e19d9236.chunk.js"
+  },
+  {
+    "revision": "05655f93960c8934c90d",
+    "url": "/Shoprosismul/static/js/main.978a88fb.chunk.js"
+  },
+  {
+    "revision": "234e7406f56fb0cf75e8",
+    "url": "/Shoprosismul/static/js/runtime~main.96928b0e.js"
+  },
+  {
+    "revision": "2a6b5abdde5f0272edf8be1b7ba267b2",
+    "url": "/Shoprosismul/static/media/anca-alexandrescu.2a6b5abd.png"
+  },
+  {
+    "revision": "85f62d4c57f2acb2a010a951e6e0e117",
+    "url": "/Shoprosismul/static/media/coiful-dacic.85f62d4c.jpg"
+  },
+  {
+    "revision": "a648ba84448aee527859939910ae0c71",
+    "url": "/Shoprosismul/static/media/presedintele-ales.a648ba84.png"
+  },
+  {
+    "revision": "da22d97b2e3a76e9b27d1e6bea3eb208",
+    "url": "/Shoprosismul/static/media/statulparalel.da22d97b.png"
+  }
+]);
